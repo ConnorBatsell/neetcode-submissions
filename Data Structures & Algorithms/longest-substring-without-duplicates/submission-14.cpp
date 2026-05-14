3c7a3320d1160c75026db0003d1ebcf43f806f8b
ConnorBatsell <cb155@rice.edu>
2026-05-13 19:46:40 -0500

    Add: longest-substring-without-duplicates - submission-14

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-14.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> temp;
+        int l=0;
+        int maxSize = 0;
+        for(int r=0; r<s.size(); r++){
+            while(temp[s[r]]>0){
+                temp.erase(s[l]);
+                l++;
+            }
+            temp[s[r]]++;
+            maxSize = max((int)temp.size(), maxSize);
+        }
+        return maxSize;
+    }
+};
