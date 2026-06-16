f2d5a574b0a96e097ffdcff0d5ee703eb69da291
ConnorBatsell <wbatsell303@gmail.com>
2026-06-16 11:32:58 -0500

    Add: meeting-rooms-iii - submission-0

---
+++ Data Structures & Algorithms/meeting-rooms-iii/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        available =[i for i in range(n)]
+        used = []
+        count = [0]*n
+        for start, end in meetings:
+            while used and start>=used[0][0]:
+                _, room = heapq.heappop(used)
+                heapq.heappush(available, room)
+            if not available:
+                endA, room = heapq.heappop(used)
+                end = endA + (end-start)
+                heapq.heappush(available, room)
+            room = heapq.heappop(available)
+            heapq.heappush(used, (end, room))
+            count[room]+=1
+        return count.index(max(count))
