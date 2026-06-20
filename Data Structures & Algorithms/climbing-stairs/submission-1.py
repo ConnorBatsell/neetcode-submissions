5b622b4c5f93eef87cc08a21f8adf4491b915c2c
Connor Batsell <wbatsell303@gmail.com>
2026-06-20 16:59:23 -0500

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        self.dp = {}
+        def helper(curr):
+            if curr in self.dp:
+                return self.dp[curr]
+            if curr<=0:
+                if curr==0:
+                    return 1
+                return 0
+            self.dp[curr] = helper(curr-2) + helper(curr-1)
+            return self.dp[curr]
+        helper(n)
+        return self.dp[n]
