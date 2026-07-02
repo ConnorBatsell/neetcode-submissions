648db24b0efe60d3196c6e25d0c259406cb945fa
Connor Batsell <wbatsell303@gmail.com>
2026-07-02 14:26:56 -0500

    Add: longest-increasing-path-in-matrix - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        dp = {}
+        rows = len(matrix)
+        cols = len(matrix[0])
+        def dfs(r,c,prev):
+            if min(r,c) < 0 or r==rows or c==cols or matrix[r][c] <= prev:
+                return 0
+            if (r,c) in dp:
+                return dp[(r,c)]
+            curr = matrix[r][c]
+            dp[(r,c)] = 1 + max(dfs(r+1,c,curr), dfs(r-1,c,curr), dfs(r,c+1,curr), dfs(r,c-1,curr))
+            return dp[(r,c)]
+        res = 0
+        for i in range(rows):
+            for j in range(cols):
+                res = max(res,dfs(i,j,-1))
+        return res
