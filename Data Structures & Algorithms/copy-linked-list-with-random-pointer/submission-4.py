df7d6dfeb0f20822e6e37ee2dd9f447b32ef80d3
Connor Batsell <wbatsell303@gmail.com>
2026-07-08 20:49:45 -0500

    Add: copy-linked-list-with-random-pointer - submission-4

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-4.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        nodes = {}
+        curr = head
+        while curr:
+            nodes[curr] = Node(curr.val)
+            curr = curr.next
+        nodes[None] = None
+        curr = head
+        while curr:
+            nodes[curr].next = nodes[curr.next]
+            nodes[curr].random = nodes[curr.random]
+            curr = curr.next
+        return nodes[head]
+    
