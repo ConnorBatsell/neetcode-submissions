7c86e0bdd1edc00d6c66ee4d09102404de6d5f5b
Connor Batsell <wbatsell303@gmail.com>
2026-07-12 11:56:01 -0500

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, val: int) -> None:
+        self.s1.append(val)
+        if self.s2:
+            self.s2.append(min(val, self.s2[-1]))
+        else:
+            self.s2.append(val)
+
+    def pop(self) -> None:
+        self.s1.pop()
+        self.s2.pop()
+        
+    def top(self) -> int:
+        return self.s1[-1]
+
+    def getMin(self) -> int:
+        return self.s2[-1]
+        
+        
