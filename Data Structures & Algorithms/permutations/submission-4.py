90f27dab4b272f0ee18465ea3a4c564564896814
Connor Batsell <wbatsell303@gmail.com>
2026-07-22 20:05:27 -0500

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        res = [[]]
+        for n in nums:
+            temp = []
+            for p in res:
+                for i in range(len(p)+1):
+                    a = p.copy()
+                    a.insert(i,n)
+                    temp.append(a)
+            res = temp
+        return res
